e1c97e0ce4d82561c6c5751cef554d588f051d55
alisterburt <alisterburt@gmail.com>
2022-12-01 18:04:58 +0000

    test connected component transform

--- src/fidder/_tests/test_utils.py
+++ src/fidder/_tests/test_utils.py
@@ -3,7 +3,7 @@ import torch
 from fidder.utils import connected_component_transform_2d
 
 
-def test_n_connected_components():
+def test_connected_component_transform_2d():
     mask = torch.zeros((10, 10))
     mask[::2, ::2] = 1
     mask[1::2, 1::2] = 1
