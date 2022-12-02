fa57e93c588ec7eb7b5682daec045fbdc8c98c38
alisterburt <alisterburt@gmail.com>
2022-12-02 01:01:00 +0000

    fix tests

--- src/fidder/erase/_tests/test_erasing.py
+++ src/fidder/erase/_tests/test_erasing.py
@@ -2,15 +2,15 @@ import pytest
 import torch
 import torch.nn.functional as F
 
-from libtilt.inpainting import inpaint
+from fidder.erase import erase_masked_region
 
 
-def test_inpaint_2d_tensor():
+def test_erase_2d():
     """Simple test that the function runs and produces output."""
     image = torch.ones((28, 28))
     mask = F.pad(torch.ones((14, 14)), pad=(7, 7, 7, 7))
     with pytest.warns(UserWarning):
-        inpainted = inpaint(
+        inpainted = erase_masked_region(
             image,
             mask,
             background_intensity_model_resolution=(5, 5),
@@ -19,12 +19,12 @@ def test_inpaint_2d_tensor():
     assert inpainted.shape == image.shape
 
 
-def test_inpaint_3d_tensor():
+def test_erase_batched_2d():
     """Simple test that the function runs and produces output."""
     image = torch.ones((2, 28, 28))
     mask = F.pad(torch.ones((2, 14, 14)), pad=(7, 7, 7, 7))
     with pytest.warns(UserWarning):
-        inpainted = inpaint(
+        inpainted = erase_masked_region(
             image=image,
             mask=mask,
             background_intensity_model_resolution=(5, 5),
