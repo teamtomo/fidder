55b515088b6cd79e06e596e2da225bebc3eb5be3
alisterburt <alisterburt@gmail.com>
2022-12-02 03:48:48 +0000

    minify model tests for CI

--- src/fidder/model/_tests/test_model.py
+++ src/fidder/model/_tests/test_model.py
@@ -11,18 +11,18 @@ def test_model_instantiation():
 def test_model_call():
     """Model should return 2-class logits of same shape."""
     model = Fidder()
-    image = torch.rand(size=(10, 1, 512, 512))
+    image = torch.rand(size=(2, 1, 128, 128))
     out = model(image)
-    assert out.shape == (10, 2, 512, 512)
+    assert out.shape == (2, 2, 128, 128)
 
 
 def test_model_predict_step():
-    """Test auto tiled prediction of large single images.
+    """Test auto tiled prediction of larger single images.
 
     model.predict_step() should yield probabilities of same shape for class 1.
     """
-    model = Fidder()
-    image = torch.rand(size=(1024, 1024))
+    model = Fidder(batch_size=1)
+    image = torch.rand(size=(512, 512))
     out = model.predict_step(image)
     assert out.shape == image.shape
     assert torch.min(out) >= 0
