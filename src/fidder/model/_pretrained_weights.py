dd938692be712b2451b2e6fa04f71ee0e010d6a4
alisterburt <alisterburt@gmail.com>
2023-02-21 14:02:58 +0000

    update reference to weights on zenodo (#16)

--- src/fidder/model/_pretrained_weights.py
+++ src/fidder/model/_pretrained_weights.py
@@ -5,8 +5,8 @@ import pooch
 # https://github.com/fatiando/pooch
 GOODBOY = pooch.create(
     path=pooch.os_cache("fidder"),
-    base_url="doi:10.5281/zenodo.7406503/",
-    registry={"fidder.ckpt": "md5:9df1775c157c5b368742e2dbc33f5b10"},
+    base_url="doi:10.5281/zenodo.7660754/",
+    registry={"fidder.ckpt": "md5:6e0eb9d3ed49a7a6a2ad65952e127ced"},
 )
 
 
